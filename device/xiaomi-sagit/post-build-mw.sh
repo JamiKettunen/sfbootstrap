4190801ec97d16d94d478a4f8658a346bbb6b3cb
Verevka <verevka86@gmail.com>
2022-06-05 23:20:35 +0300

    xiaomi-sagit: Drop storeman

--- device/xiaomi-sagit/post-build-mw.sh
+++ device/xiaomi-sagit/post-build-mw.sh
@@ -1,3 +1,2 @@
 # Additional MW for extra functionality on Xiaomi MI 6 devices
-sfb_build_packages --mw=https://github.com/mentaljam/harbour-storeman.git && \
 sfb_build_packages --mw=sailfish-fpd-community --spec=rpm/droid-biometry-fp.spec --spec=rpm/sailfish-fpd-community.spec
